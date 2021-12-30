bc7c581909c309e98ff405a6abb30517566ed630
Jean <nguyen.jean@me.com>
2021-12-30 13:40:55 +0100

    add placeholder for input playlist name

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,6 @@
 const clienId = '86b48da2759146e0925b12937fd49eb3';
 const redirectUri = 'https://jeanmming.surge.sh';
+//const redirectUri = 'http://localhost:3000/';
 let accessToken;
 
 const Spotify = {
